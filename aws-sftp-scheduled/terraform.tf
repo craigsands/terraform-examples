9a03602052ed3dde76a86e03fc725d7431a0b3ce
Craig Sands <craig.sands@gmail.com>
2025-04-23 22:52:13 -0400

    Remove local as required provider

--- aws-sftp-scheduled/terraform.tf
+++ aws-sftp-scheduled/terraform.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.95.0"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "2.5.1"
-    }
     tls = {
       source  = "hashicorp/tls"
       version = "4.0.6"
